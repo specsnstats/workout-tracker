f313fac46ccc00b2d0235e8d97f7e41364c50486
SpecsNStats <specsnstats@gmail.com>
2021-11-18 15:31:34 -0700

    changes for mongo

--- server.js
+++ server.js
@@ -17,7 +17,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{ 
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+    useCreateIndex: true,
+    useFindAndModify:true
+ });
 
 app.get("/:file", (req,res)=>{
     res.sendFile(__dirname + "\\public\\" + req.params.file + ".html");
